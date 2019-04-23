55b3ea0d8952dfe87fef37a4de53d5f5369bfbc0
“Ranjit <ranjit.saimbi@gmail.com>
2019-04-23 18:38:35 +0100

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet (time) {
+  var text = time.slice(0,2);
+  var integer = parseInt(text, 10);
+
+  if (integer < 12) {
+    return 'Good Morning';
+  } else if (integer >= 12 && integer < 17) {
+    return 'Good Afternoon'
+  } else if (integer >= 17 && integer < 24) {
+    return 'Good Evening';
+  }
+}
+
+function displayMessage(greetString) {
+  document.querySelector('h1#greeting').innerHTML = greetString;
+};
