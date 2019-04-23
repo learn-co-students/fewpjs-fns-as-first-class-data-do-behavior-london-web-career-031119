82570a74d5587571fc107ee0056ac4c2fa5db7d0
SANTOrick <riccardo_london@live.it>
2019-04-23 13:51:24 +0100

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
 
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time){
+
+const thistime = parseInt(time, 10);
+if(thistime < 12) return "Good Morning"
+if(thistime > 18) return "Good Evening"
+return "Good Afternoon"
+}
