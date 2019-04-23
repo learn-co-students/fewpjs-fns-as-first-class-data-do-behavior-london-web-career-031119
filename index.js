1dc3d93d1cfd7bbda8ca47f6368ad3221c40a9b6
Manon Jacquin <manon.jacquin@gmail.com>
2019-04-23 14:25:20 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  let splitted =timeString.split(':')
+  let hours = parseInt(splitted[0])
+  // let minutes = parseInt(splitted[1]) 
+
+  if (hours < 12) {
+    return 'Good Morning';
+  } 
+  else if (hours > 17) {
+    return 'Good Evening'
+  }
+  else {
+    return 'Good Afternoon'
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  const greeting = document.getElementById('greeting');
+  greeting.innerHTML = str; 
+}
