e1f005d3fbe0e6c5bf2933cbcd57a51d68c53560
mrsalvini <dsgmcosta@gmail.com>
2019-04-23 15:32:57 +0100

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-/* Given Code, don't edit */
-
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
+}
+
+function greet(time) {
+  if (parseInt(time) < 12) {
+    return "Good Morning"
+  }
+  else if (parseInt(time) >= 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
+function displayMessage(msg) {
+  document.querySelector("#greeting").textContent = msg
 }
 
 /* End Given Code, don't edit above here...*/
