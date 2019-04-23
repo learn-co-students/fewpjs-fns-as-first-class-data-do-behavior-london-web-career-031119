dc51b1b8fd658469c621760a129724d74a8dd59f
oli-gra <oliver.grant@gmail.com>
2019-04-23 22:57:39 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let t = parseInt(time.slice(0,2))
+  console.log(t)
+  if (t < 12) { return "Good Morning" }
+    else if (t > 17) { return "Good Evening" }
+    else { return "Good Afternoon" }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  let h1 = document.querySelector('h1#greeting')
+  h1.textContent = string
+}
